5d676f3d792ef874fae805cc1c4903ee8b4d02d1
DeneIs <55320251+A00433151@users.noreply.github.com>
2020-02-04 13:09:47 -0400

    Add files via upload

---
+++ Coffee@desk/Controllers/Class1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Coffee_desk.Models;
+
+namespace Coffee_desk.Controllers
+{
+    public class Class1Controller : Controller
+    {
+        // GET: Class1
+        public ActionResult Random()
+        {
+            var class1 = new Class1() { Name = "shrek" };
+            // to return view
+            //return View(class1);
+            // to redirect
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        //localhost:44339/Class1/edit?id=2
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Coffee-desk
+Coffee@desk proejct with team members
+
+Build on ASP.net MVC5 framework and is currently deployed on Azure platform (https://coffeedesk.azurewebsites.net/)
+This is a small web platform to order coffee from some outlets at designated location and pay with credit cards that check for few validations.
+Also admin page to check for any existing orders (https://coffeedesk.azurewebsites.net/admin) only works when database is up and running.
